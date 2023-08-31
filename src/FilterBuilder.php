8b4009b5fdfea0b014262c601c881036804e0ac1
Kayck Matias <kayckmatias04@gmail.com>
2023-08-31 10:11:39 -0300

    [feat] added new relation filters and refactor filters array

--- src/FilterBuilder.php
+++ src/FilterBuilder.php
@@ -2,6 +2,8 @@
 
 namespace Kayckmatias\ZFilters;
 
+use Illuminate\Database\Eloquent\Builder;
+
 class FilterBuilder
 {
     protected $query;
@@ -13,21 +15,36 @@ class FilterBuilder
         $this->filters = $filters;
     }
 
-    protected function filtersMake($simple, $complex)
+    protected function filtersMake(array $filters): Builder
     {
-        foreach($this->filters as $key => $value) {
-            if(!$value){
+        [$simple, $relation, $complex] = $filters;
+
+        foreach ($this->filters as $key => $value) {
+            if (!$value) {
                 continue;
             }
 
-            if($this->checkFilterAvailable($key, $simple)) {
-                $this->query->whereIn($simple[$key], $this->formatSimpleValue($value));
+            if ($this->checkFilterAvailable($key, $simple)) {
+                $this->query
+                    ->whereIn($simple[$key], $this->formatSimpleValue($value));
             }
 
-            if($this->checkFilterAvailable($key, $complex)) {
+            if ($this->checkFilterAvailable($key, $relation)) {
+                $relationFilter = key($relation[$key]);
+                $column = $relation[$key][$relationFilter];
+                $formattedValue = $this->formatSimpleValue($value);
+
+                $this->query->whereHas($relationFilter, function ($q) use ($column, $formattedValue) {
+                    $q->whereIn($column, $formattedValue);
+                });
+            }
+
+            if ($this->checkFilterAvailable($key, $complex)) {
                 $complex[$key]($this->query, $value);
             }
         }
+
+        return $this->query;
     }
 
     private function formatSimpleValue($value){
